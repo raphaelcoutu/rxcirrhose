17368c3ee3f5848b22b70b7340c748a9e1de6472
Raphaël Coutu <raph244@gmail.com>
2018-06-02 12:10:46 -0400

    Update DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Article;
+use App\Drug;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +15,30 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@rxcirrhose.com',
+            'password' => '$2y$10$pX4i3tsBCC03RTSlVxeRFufu4AjtGfpQwM7LxuddLnzrMwv0w2yT2'
+        ]);
+
+        Article::create([
+            'title' => 'Biguanides'
+        ]);
+
+        Drug::create([
+            'article_id' => 1,
+            'name' => 'Metformine',
+            'childA_category' => 1,
+            'childB_category' => 2,
+            'childC_category' => 3
+        ]);
+
+        Drug::create([
+            'article_id' => 1,
+            'name' => 'Metamorfine',
+            'childA_category' => 2,
+            'childB_category' => 3,
+            'childC_category' => 4
+        ]);
     }
 }
