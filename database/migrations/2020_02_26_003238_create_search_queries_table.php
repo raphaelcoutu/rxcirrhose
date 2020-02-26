223f828e7ce6e4f2dc1e99709ef4593fcfe0bb69
Raphaël Coutu <raph244@gmail.com>
2020-02-26 10:47:16 -0500

    Ajout historique de recherche

---
+++ database/migrations/2020_02_26_003238_create_search_queries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSearchQueriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('search_queries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('query');
+            $table->integer('results');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('search_queries');
+    }
+}
