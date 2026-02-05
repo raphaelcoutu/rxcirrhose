c8fd482bc12dc34a16aebd474fd1795182c4166a
Raphaël Coutu <raph244@gmail.com>
2026-02-04 19:51:22 -0500

    fix dark mode

--- resources/views/pages/about_us_en.blade.php
+++ resources/views/pages/about_us_en.blade.php
@@ -10,36 +10,36 @@
 @section('content')
     @include('layouts.navbar', ['active' => ''])
     <div class="mx-auto container flex flex-grow justify-between items-start pt-4 leading-loose tracking-normal">
-        <div class="mx-auto w-full sm:w-3/4 bg-white border-t-8 border-red px-6 py-3 shadow">
-            <div class="flex border-b border-grey-light items-center">
-                <h1 class="text-3xl leading-none text-red">About Us</h1>
+        <div class="mx-auto w-full sm:w-3/4 bg-white border-t-8 border-red px-6 py-3 shadow dark:bg-slate-800 dark:border-red-500">
+            <div class="flex border-b border-gray-400 items-center dark:border-slate-700">
+                <h1 class="text-3xl leading-none text-red-500 dark:text-red-400">About Us</h1>
             </div>
-            <h3 class="mt-4 px-2 bg-grey-lightest border-t border-b border-grey-light">Webmasters</h3>
+            <h3 class="mt-4 px-2 bg-gray-100 border-t border-b border-gray-400 dark:bg-slate-800 dark:border-slate-700">Webmasters</h3>
 
             <div class="pl-2">
-                <div class="border-b-2 border-grey-lighter">
-                    <div class="text-red-darker">Raphaël Coutu, Pharm.D, M.Sc.</div>
-                    <div class="pl-2 text-grey-darker text-sm leading-none">Pharmacist at CIUSSSE-CHUS, CHUS site</div>
-                    <div class="pl-2 text-sm">Review of drug datasheets</div>
+                <div class="border-b-2 border-gray-200 dark:border-slate-700">
+                    <div class="text-red-800 dark:text-red-400">Raphaël Coutu, Pharm.D, M.Sc.</div>
+                    <div class="pl-2 text-gray-800 text-sm leading-none dark:text-gray-300">Pharmacist at CIUSSSE-CHUS, CHUS site</div>
+                    <div class="pl-2 text-sm dark:text-gray-400">Review of drug datasheets</div>
                 </div>
-                <div class="border-b-2 border-grey-lighter">
-                    <div class="text-red-darker">Victor Ferreira, Pharm.D, M.Sc.</div>
-                    <div class="pl-2 text-grey-darker text-sm leading-none">Pharmacist at CHUM</div>
-                    <div class="pl-2 text-sm">Writing and review of drug datasheets</div>
+                <div class="border-b-2 border-gray-200 dark:border-slate-700">
+                    <div class="text-red-800 dark:text-red-400">Victor Ferreira, Pharm.D, M.Sc.</div>
+                    <div class="pl-2 text-gray-800 text-sm leading-none dark:text-gray-300">Pharmacist at CHUM</div>
+                    <div class="pl-2 text-sm dark:text-gray-400">Writing and review of drug datasheets</div>
                 </div>
-                <div class="border-b-2 border-grey-lighter">
-                    <div class="text-red-darker">Alexandre Mathieu, Pharm.D, M.Sc.</div>
-                    <div class="pl-2 text-grey-darker text-sm leading-none">Pharmacist at CISSS de Lanaudière</div>
-                    <div class="pl-2 text-sm">Writing and review of drug datasheets</div>
+                <div class="border-b-2 border-gray-200 dark:border-slate-700">
+                    <div class="text-red-800 dark:text-red-400">Alexandre Mathieu, Pharm.D, M.Sc.</div>
+                    <div class="pl-2 text-gray-800 text-sm leading-none dark:text-gray-300">Pharmacist at CISSS de Lanaudière</div>
+                    <div class="pl-2 text-sm dark:text-gray-400">Writing and review of drug datasheets</div>
                 </div>
                 <div>
-                    <div class="text-red-darker">D<sup>r</sup> Julien Bissonnette, MD</div>
-                    <div class="pl-2 text-grey-darker text-sm leading-none">Hepatologist at CHUM</div>
-                    <div class="pl-2 text-sm">Review of drug datasheets</div>
+                    <div class="text-red-800 dark:text-red-400">D<sup>r</sup> Julien Bissonnette, MD</div>
+                    <div class="pl-2 text-gray-800 text-sm leading-none dark:text-gray-300">Hepatologist at CHUM</div>
+                    <div class="pl-2 text-sm dark:text-gray-400">Review of drug datasheets</div>
                 </div>
                 </div>
 
-            <h3 class="mt-4 px-2 bg-grey-lightest border-t border-b border-grey-light">Collaborators</h3>
+            <h3 class="mt-4 px-2 bg-gray-100 border-t border-b border-gray-400 dark:bg-slate-800 dark:border-slate-700">Collaborators</h3>
             <ul>
                 <li>Yasemin Cokay, candidate for Pharm.D (2018): Writing of drug datasheets</li>
                 <li>Steven Mancino, candidate for Pharm.D (2018): Writing of drug datasheets</li>
--- resources/views/pages/about_us_fr.blade.php
+++ resources/views/pages/about_us_fr.blade.php
@@ -10,36 +10,36 @@
 @section('content')
     @include('layouts.navbar', ['active' => ''])
     <div class="mx-auto container flex flex-grow justify-between items-start pt-4 leading-loose tracking-normal">
-        <div class="mx-auto w-full sm:w-3/4 bg-white border-t-8 border-red-500 px-6 py-3 shadow">
-            <div class="flex border-b border-gray-400 items-center">
+        <div class="mx-auto w-full sm:w-3/4 bg-white border-t-8 border-red-500 px-6 py-3 shadow dark:bg-slate-800 dark:border-red-500">
+            <div class="flex border-b border-gray-400 items-center dark:border-slate-700">
                 <h1 class="text-3xl leading-none text-red-500">Qui Sommes-Nous?</h1>
             </div>
-            <h3 class="mt-4 px-2 bg-gray-100 border-t border-b border-gray-400">Webmaîtres</h3>
+            <h3 class="mt-4 px-2 bg-gray-100 border-t border-b border-gray-400 dark:bg-slate-800 dark:border-slate-700">Webmaîtres</h3>
 
             <div class="pl-2">
-                <div class="border-b-2 border-gray-200">
-                    <div class="text-red-800">Raphaël Coutu, Pharm.D, M.Sc.</div>
-                    <div class="pl-2 text-gray-800 text-sm leading-none">Pharmacien au CIUSSSE-CHUS, site CHUS</div>
-                    <div class="pl-2 text-sm">Révision des fiches</div>
+                <div class="border-b-2 border-gray-200 dark:border-slate-700">
+                    <div class="text-red-800 dark:text-red-400">Raphaël Coutu, Pharm.D, M.Sc.</div>
+                    <div class="pl-2 text-gray-800 text-sm leading-none dark:text-gray-300">Pharmacien au CIUSSSE-CHUS, site CHUS</div>
+                    <div class="pl-2 text-sm dark:text-gray-400">Révision des fiches</div>
                 </div>
-                <div class="border-b-2 border-gray-200">
-                    <div class="text-red-800">Victor Ferreira, Pharm.D, M.Sc.</div>
-                    <div class="pl-2 text-gray-800 text-sm leading-none">Pharmacien au CHUM</div>
-                    <div class="pl-2 text-sm">Rédaction et révision de fiches</div>
+                <div class="border-b-2 border-gray-200 dark:border-slate-700">
+                    <div class="text-red-800 dark:text-red-400">Victor Ferreira, Pharm.D, M.Sc.</div>
+                    <div class="pl-2 text-gray-800 text-sm leading-none dark:text-gray-300">Pharmacien au CHUM</div>
+                    <div class="pl-2 text-sm dark:text-gray-400">Rédaction et révision de fiches</div>
                 </div>
-                <div class="border-b-2 border-gray-200">
-                    <div class="text-red-800">Alexandre Mathieu, Pharm.D, M.Sc.</div>
-                    <div class="pl-2 text-gray-800 text-sm leading-none">Pharmacien au CISSS de Lanaudière</div>
-                    <div class="pl-2 text-sm">Rédaction et révision de fiches</div>
+                <div class="border-b-2 border-gray-200 dark:border-slate-700">
+                    <div class="text-red-800 dark:text-red-400">Alexandre Mathieu, Pharm.D, M.Sc.</div>
+                    <div class="pl-2 text-gray-800 text-sm leading-none dark:text-gray-300">Pharmacien au CISSS de Lanaudière</div>
+                    <div class="pl-2 text-sm dark:text-gray-400">Rédaction et révision de fiches</div>
                 </div>
                 <div>
-                    <div class="text-red-800">D<sup>r</sup> Julien Bissonnette, MD</div>
-                    <div class="pl-2 text-gray-800 text-sm leading-none">Hépatologue au CHUM</div>
-                    <div class="pl-2 text-sm">Révision des fiches</div>
+                    <div class="text-red-800 dark:text-red-400">D<sup>r</sup> Julien Bissonnette, MD</div>
+                    <div class="pl-2 text-gray-800 text-sm leading-none dark:text-gray-300">Hépatologue au CHUM</div>
+                    <div class="pl-2 text-sm dark:text-gray-400">Révision des fiches</div>
                 </div>
                 </div>
 
-            <h3 class="mt-4 px-2 bg-gray-100 border-t border-b border-gray-400">Collaborateurs</h3>
+            <h3 class="mt-4 px-2 bg-gray-100 border-t border-b border-gray-400 dark:bg-slate-800 dark:border-slate-700">Collaborateurs</h3>
             <ul>
                 <li>Yasemin Cokay, candidate au Pharm.D (2018): Rédaction de fiches</li>
                 <li>Steven Mancino, candidat au Pharm.D (2018): Rédaction de fiches</li>
